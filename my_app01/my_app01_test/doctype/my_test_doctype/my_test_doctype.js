3d670c7bc4246eed8b22549c00c915d561d0048b
Justin Clark Ong <122538996+justinclarkong@users.noreply.github.com>
2024-10-11 10:52:56 +0800

    Revert "This is a test doctype"

--- my_app01/my_app01_test/doctype/my_test_doctype/my_test_doctype.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2024, Justin Clark Ong and contributors
-// For license information, please see license.txt
-
-// frappe.ui.form.on("My Test DocType", {
-// 	refresh(frm) {
-
-// 	},
-// });
